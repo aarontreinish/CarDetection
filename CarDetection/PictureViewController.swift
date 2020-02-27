8b49b10bebf51005d649cb5a9a28fe5f05130621
Aaron Treinish <atreinish@kwi.com>
2020-02-26 19:20:02 -0500

    Improving api request and UI

--- CarDetection/PictureViewController.swift
+++ CarDetection/PictureViewController.swift
@@ -13,14 +13,24 @@ class PictureViewController: UIViewController {
     @IBOutlet weak var pictureImageView: UIImageView!
     @IBOutlet weak var exitButton: UIButton!
     @IBOutlet weak var makeLabel: UILabel!
+    @IBOutlet weak var labelView: UIView!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
     
     var takenImage = UIImage()
     
     var carNetResponse: CarNET?
     
+    let activityIndicator = UIActivityIndicatorView()
+    
+    var screenWillShow = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        pictureImageView.image = UIImage(named: "gen coupe")
+        //pictureImageView.image = takenImage
+        
         carNetRequest()
         
     }
@@ -28,8 +38,32 @@ class PictureViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        setupActivityIndicator()
         
-        pictureImageView.image = takenImage
+        if screenWillShow == false {
+            pictureImageView.isHidden = true
+            exitButton.isHidden = true
+            labelView.isHidden = true
+            activityIndicator.startAnimating()
+        }
+
+    }
+    
+    func setupActivityIndicator() {
+        view.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.hidesWhenStopped = true
+        let horizontalConstraint = NSLayoutConstraint(item: activityIndicator, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 0)
+        view.addConstraint(horizontalConstraint)
+        let verticalConstraint = NSLayoutConstraint(item: activityIndicator, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: 0)
+        view.addConstraint(verticalConstraint)
+        
+        if #available(iOS 13.0, *) {
+            activityIndicator.style = .large
+        } else {
+            activityIndicator.transform = CGAffineTransform.init(scaleX: 1.5, y: 1.5)
+            activityIndicator.color = UIColor.black
+        }
         
     }
     
@@ -39,7 +73,7 @@ class PictureViewController: UIViewController {
             "Content-Type": "application/octet-stream",
             "api-key": "9e52c29a-615a-4813-a8c2-421cbb43ace3",
         ]
-        let photo = takenImage.jpegData(compressionQuality: 0.5)
+        let photo = pictureImageView.image?.jpegData(compressionQuality: 0.5)
         let request = NSMutableURLRequest(url: NSURL(string: "https://api.carnet.ai/v2/mmg/detect")! as URL,
                                           cachePolicy: .useProtocolCachePolicy,
                                           timeoutInterval: 10.0)
@@ -65,6 +99,11 @@ class PictureViewController: UIViewController {
                 self.carNetResponse = data
                 
                 DispatchQueue.main.async {
+                    self.screenWillShow = true
+                    self.activityIndicator.stopAnimating()
+                    self.pictureImageView.isHidden = false
+                    self.exitButton.isHidden = false
+                    self.labelView.isHidden = false
                     self.setLabels()
                 }
                 
@@ -84,6 +123,8 @@ class PictureViewController: UIViewController {
         for mmg in carNetResponse?.detections ?? [] {
             for make in mmg.mmg ?? [] {
                 makeLabel.text = make.make_name
+                modelLabel.text = make.model_name
+                yearLabel.text = make.years
             }
         }
     }
